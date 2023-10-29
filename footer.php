9d5a807ef78ebd725533e450e919d47856ca0095
Sebastian <93062325+Sebaexeq@users.noreply.github.com>
2023-10-29 02:57:26 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,11 @@
-<footer class="mt-4" style="background-color: #102C57; padding: 20px 0;"> <!-- Color más oscuro de la paleta -->
-<!-- Agregar el botón en el lugar donde deseas que aparezca inicialmente -->
+<footer class="mt-4" style="background-color: #102C57; padding: 20px 0;">
 <a href="#" id="irArribaBtn" style="position: fixed; bottom: 20px; right: 20px; display: none;">
     <img src="flecha.png" title="Ir arriba" width="50" height="50" alt="Ir arriba" />
 </a>
     <div class="container">
         <div class="row">
             <div class="col-md-6 text-center text-md-left mb-3 mb-md-0">
-                <p style="color: #F8F0E5;">&copy; <?php echo date("Y"); ?> RapiBnB</p> <!-- Color más claro de la paleta -->
+                <p style="color: #F8F0E5;">&copy; <?php echo date("Y"); ?> RapiBnB</p>
             </div>
             <div class="col-md-6 text-center text-md-right">
                 <a href="#" style="color: #EADBC8; margin-right: 10px;"><i class="bi bi-facebook" style="font-size: 24px;"></i></a> <!-- Segundo color de la paleta -->
